df96dd450c71fe088a209002adf06c2063c7bd0a
Marcel-Cathelin <marcel671@gmail.com>
2021-04-15 20:33:10 +0200

    correct errors on plats.html.twig

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+
+class MenuController extends AbstractController
+
+
+{
+    public function index(): string
+    {
+
+        return $this->twig->render('Menu/index.html.twig');
+    }
+
+    public function plats(): string
+    {
+
+        return $this->twig->render('Menu/plats.html.twig');
+    }
+    public function entrees(): string
+    {
+
+        return $this->twig->render('Menu/entrees.html.twig');
+    }
+
+    public function desserts(): string
+    {
+
+        return $this->twig->render('Menu/desserts.html.twig');
+    }
+
+}
