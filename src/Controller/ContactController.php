74501a79293fc70766424757e3d4f3b0e4fb2d0e
Lulu-Soso <lucas.sokha@gmail.com>
2021-04-16 13:18:01 +0200

    delete commented code in ContactController.php

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -14,36 +14,6 @@ class ContactController extends AbstractController
             $email = $_POST['email'];
             $comment = $_POST['comment'];
 
-            //ajout erreurs
-            /*$validate =true;
-            if(empty($_POST['firstname'])){
-                $erreur_firstname = "Veuillez indiquer votre prénom.";
-                $validate = false;
-            }
-
-            if (empty($_POST['lastname'])){
-                $erreur_lastname = "Veuillez indiquer votre nom.";
-                $validate = false;
-            }
-
-            if(empty($_POST['email'])){
-                $erreur_email = "Veuillez indiquer votre email .";
-                $validate = false;
-            }elseif(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
-                    $erreur_email = "Adresse Email non valide !";
-                    $validate = false;
-                }
-
-            if(empty($_POST['comment'])){
-                $erreur_comment = "Veuillez entrer votre message";
-                $validate = false;
-            }elseif(strlen($_POST['comment']) < 5){
-                $erreur_comment = "Le message doit contenir plus de 5 caractères.";
-                $validate = false;
-            }*/
-
-            // fin messages erreurs
-
             $manager = new ContactManager();
             $manager->insert($firstname, $lastname, $email, $comment);
             header('Location:/Contact/list');
@@ -54,11 +24,6 @@ class ContactController extends AbstractController
     public function list(): string
     {
         $contacts = (new ContactManager())->selectAll();
-
-        /*
-        équivalent à
-        $manager = new ContactManager();
-        $contacts = $manager->selectAll();*/
 
         return $this->twig->render('Contact/list.html.twig', [
             'contacts' => $contacts
