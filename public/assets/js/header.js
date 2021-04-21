6ae26ef776bbde92d344acace372cc0a65ddb95a
NDIAYE <64037332+Bachir-Ndiaye@users.noreply.github.com>
2021-04-21 17:20:32 +0200

    Us header footer (#18)
    * header in all pages of the project and the hamburger form of the navbar
    
    * rectification for the position of the file of the header
    
    Co-authored-by: said <saidhoubebe@gmail.com>

---
+++ public/assets/js/header.js
@@ -0,0 +1,8 @@
+// On click Hamburger => display menu vertical
+
+const btnMenu = document.querySelector('.logo-menu');
+const menu = document.querySelector('.liste-nav');
+
+btnMenu.addEventListener('click', function(){
+menu.classList.toggle('active');
+})
