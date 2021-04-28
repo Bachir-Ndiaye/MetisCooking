f2315d07711efdd17f962277aeb953db62e65f80
Lulu-Soso <lucas.sokha@gmail.com>
2021-04-28 14:19:05 +0200

    displays errors if user already subscribe

--- src/Model/SignupManager.php
+++ src/Model/SignupManager.php
@@ -7,10 +7,36 @@ class SignupManager extends AbstractManager
 
     public const TABLE = 'users';
 
-    public function insert($firstname, $lastname, $email, $password): string
+    public function insert1($firstname, $lastname, $email, $password): string
     {
         $query = 'INSERT INTO users (firstname, lastname, email, password)
                     VALUES (:firstname, :lastname, :email, :password);';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
+        $statement->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
+        $statement->bindValue(':email', $email, \PDO::PARAM_STR);
+        $statement->bindValue(':password', $password, \PDO::PARAM_STR);
+
+        $statement->execute();
+
+        return $this->pdo->lastInsertId();
+    }
+
+    public function insert($firstname, $lastname, $email, $password): string
+    {
+
+        $query = 'SELECT firstname, lastname, email, password FROM users WHERE email= ?';
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([$email]);
+        $result = $statement->rowcount();
+
+        if ($result > 0) {
+            return '0';
+        }
+
+        $query = 'INSERT INTO users (firstname, lastname, email, password)
+                    VALUES (:firstname, :lastname, :email, :password);';
+
         $statement = $this->pdo->prepare($query);
         $statement->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
         $statement->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
